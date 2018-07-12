7d860738c2362efa563891e2e2a2ed38b9266bfc
musi <957028613@qq.com>
2018-07-13 01:29:41 +0800

    更新导航栏的功能

--- main.js
+++ main.js
@@ -10,6 +10,29 @@ window.onscroll = function() {
   }
 }
 
+let liTags = document.querySelectorAll('nav.menu > ul > li')
+for (let i = 0; i < liTags.length; i++) {
+  liTags[i].onmouseenter = function(even) {
+    even.currentTarget.classList.add("active")
+  }
+  liTags[i].onmouseleave = function(even) {
+    even.currentTarget.classList.remove("active")
+  }
+}
+
+let aTags = document.querySelectorAll('nav.menu > ul > li > a')
+for (let i = 0; i < aTags.length; i++) {
+  aTags[i].onclick = function(even) {
+    even.preventDefault()
+    let e = even.currentTarget
+    let href = e.getAttribute('href')
+    let element = document.querySelector(href)
+    let top = element.offsetTop
+    window.scrollTo(0, top - 100)
+  }
+}
+
+
 portfolio1.onclick = function() {
   portfolioBar.className = 'bar state-1'
 }
