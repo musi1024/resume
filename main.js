7070d57eec516e4063f3f75efc494d37173dc93c
musi <957028613@qq.com>
2018-07-14 01:11:24 +0800

    更新页面动态效果

--- main.js
+++ main.js
@@ -1,13 +1,42 @@
 setTimeout(function() {
   welcome.classList.remove("active")
-},1500)
+},2500)
 
+let mainBlock = document.querySelectorAll(".mainBlock")
+for (let i = 0; i < mainBlock.length; i++) {
+  mainBlock[i].classList.add('offset')
+}
+
+setTimeout(function() {
+  scrollAnimate()
+}, 2501)
 window.onscroll = function() {
   if (window.scrollY > 0) {
     topNavBar.classList.add("sticky")
   } else {
     topNavBar.classList.remove("sticky")
   }
+  scrollAnimate()
+  
+}
+
+function scrollAnimate() {
+  let mainBlock = document.querySelectorAll(".mainBlock")
+  let minIndex = 0
+  for (let i = 0; i < mainBlock.length; i++) {
+    if (Math.abs(mainBlock[i].offsetTop - window.scrollY) < Math.abs(mainBlock[minIndex].offsetTop -window.scrollY)) {
+      minIndex = i
+    }
+  }
+  mainBlock[minIndex].classList.remove('offset')
+  let id = mainBlock[minIndex].id
+  let a = document.querySelector('a[href="#' + id + '"]')
+  let li = a.parentNode
+  let brothers = li.parentNode.children
+  for(let i = 0; i < brothers.length; i++){
+    brothers[i].classList.remove('highlight')
+  }
+  li.classList.add('highlight')
 }
 
 let liTags = document.querySelectorAll('nav.menu > ul > li')
@@ -20,6 +49,12 @@ for (let i = 0; i < liTags.length; i++) {
   }
 }
 
+function animate(time) {
+  requestAnimationFrame(animate);
+  TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+
 let aTags = document.querySelectorAll('nav.menu > ul > li > a')
 for (let i = 0; i < aTags.length; i++) {
   aTags[i].onclick = function(even) {
@@ -28,7 +63,22 @@ for (let i = 0; i < aTags.length; i++) {
     let href = e.getAttribute('href')
     let element = document.querySelector(href)
     let top = element.offsetTop
-    window.scrollTo(0, top - 100)
+    
+    let currrentTop = window.scrollY
+    let targetTop = top - 100
+    let s = targetTop - currrentTop
+    var coords = { y: currrentTop }
+    var t = Math.abs((s / 100) * 300 )
+    if (t > 500) {
+      t = 500
+    }
+    var tween = new TWEEN.Tween(coords) 
+    .to({ y: targetTop }, t)
+    .easing(TWEEN.Easing.Quadratic.InOut) 
+    .onUpdate(function() { 
+      window.scrollTo(0, coords.y)
+    })
+    .start()
   }
 }
 
